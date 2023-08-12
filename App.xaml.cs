612cd3fc67c5f891dfc0d84320ad1b200d1e1132
PlainsVillager <littlecuckoo@outlook.com>
2023-08-12 19:08:23 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Linear_equation_in_two_unknowns;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
